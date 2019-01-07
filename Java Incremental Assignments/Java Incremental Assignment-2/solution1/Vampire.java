c6458fee738427c367a66abe725d383503b9787d
kp1181 <p.krishnaprasad007@gmail.com>
2019-01-07 10:54:23 +0530

    Add files via upload

--- Java Incremental Assignments/Java Incremental Assignment-2/solution1/Vampire.java
+++ Java Incremental Assignments/Java Incremental Assignment-2/solution1/Vampire.java
@@ -1,67 +1,39 @@
 package assignment2.solution1;
 
-import java.util.ArrayList;
-/*
-A vampire number v is a number with an even number of digits n, that can be factored into two numbers x and y
-each with n/2 digits and not both with trailing zeroes, where v contains precisely all the digits from x and from y,
- in any order. Write a java program to print first 100 vampire numbers.
- */
+import java.util.Arrays;
+import java.util.HashSet;
 
+public class Vampire{
+    private static int numDigits(long num){
+        return Long.toString(Math.abs(num)).length();
+    }
 
-public class Vampire {
+    private static boolean fangCheck(long orig, long fang1, long fang2){
+        if(Long.toString(fang1).endsWith("0") && Long.toString(fang2).endsWith("0")) return false;
 
-    static ArrayList b = new ArrayList();
-    public static void main(String args[])
-    {
-        int count=0;
+        int origLen = numDigits(orig);
+        if(numDigits(fang1) != origLen / 2 || numDigits(fang2) != origLen / 2) return false;
 
-        for(int i=0;count<100;i++)
-        {
-            if(vampire(i)) {
-                System.out.println(i);
+        byte[] origBytes = Long.toString(orig).getBytes();
+        byte[] fangBytes = (Long.toString(fang1) + Long.toString(fang2)).getBytes();
+        Arrays.sort(origBytes);
+        Arrays.sort(fangBytes);
+        return Arrays.equals(origBytes, fangBytes);
+    }
+
+    public static void main(String[] args){
+        HashSet<Long> vamps = new HashSet<Long>();
+        for(long i = 10; vamps.size() <= 100; i++ ){
+            if((numDigits(i) % 2) != 0) {i = i * 10 - 1; continue;}
+            for(long fang1 = 2; fang1 <= Math.sqrt(i) + 1; fang1++){
+                if(i % fang1 == 0){
+                    long fang2 = i / fang1;
+                    if(fangCheck(i, fang1, fang2) && fang1 <= fang2){
+                        vamps.add(i);
+                        System.out.println(i + ": [" + fang1 + ", " + fang2 +"]");
+                    }
+                }
             }
-
         }
-    }
-
-
-    static void permute(char[] charNum,String prefix,int n,int k)
-    {
-
-        if(k==0)
-        {
-            int num = Integer.parseInt(prefix);
-            b.add(num);
-            return;
-        }
-        for(int i=0;i<n;i++)
-        {
-            String newPrefix = prefix + charNum[i];
-            permute(charNum,newPrefix,n,k-1);
-        }
-    }
-
-
-    static boolean vampire(int num)
-    {
-        String strNum = String.valueOf(num);
-        int n = strNum.length();
-        if(n%2!=0 || n==2)
-        {
-            return false;
-        }
-
-
-        //permutations of length n/2
-        int l = n/2;
-        ArrayList a = new ArrayList();
-        char[] charNum = strNum.toCharArray();
-
-        permute(charNum,"",n,n/2);
-        System.out.println("permuting "+ strNum);
-        System.out.println(b);
-
-        return true;
-
     }
 }
