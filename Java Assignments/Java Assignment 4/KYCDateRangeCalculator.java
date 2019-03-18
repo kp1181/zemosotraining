fd3c9c132a222f41cdeb982cb45cd1e4244b6e27
kp1181 <p.krishnaprasad007@gmail.com>
2019-03-18 15:36:38 +0530

    Update KYCDateRangeCalculator.java

--- Java Assignments/Java Assignment 4/KYCDateRangeCalculator.java
+++ Java Assignments/Java Assignment 4/KYCDateRangeCalculator.java
@@ -1,13 +1,16 @@
-package assignmentn4;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.time.format.ResolverStyle;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/*
+    The KYCDateRangeCalculator class calculates the date range in which
+    the KYC form can be filled by the customer.
 
+
+    @author Krishna Prasad
+ */
 public class KYCDateRangeCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -17,8 +20,17 @@ public class KYCDateRangeCalculator {
                 String signUpDateString = sc.next();
                 String currentDateString = sc.next();
 
-                DateTimeFormatter givenDateFormat=DateTimeFormatter.ofPattern("dd-MM-uuuu").withResolverStyle(ResolverStyle.STRICT);
-                findRange(signUpDateString,currentDateString,givenDateFormat);
+                DateTimeFormatter givenDateFormat=DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+                LocalDate signUpDate = LocalDate.parse(signUpDateString,givenDateFormat);
+                LocalDate currentDate = LocalDate.parse(currentDateString,givenDateFormat);
+
+                if(currentDate.getYear() <= signUpDate.getYear()){
+                    noRange();
+                    continue;
+                }
+
+                findRange(signUpDate,currentDate,givenDateFormat);
             }
 
         }
@@ -32,27 +44,27 @@ public class KYCDateRangeCalculator {
 
 
 
-    static void findRange(String signUpDateString,String currentDateString,DateTimeFormatter givenDateFormat){
 
 
-        LocalDate signUpDate = LocalDate.parse(signUpDateString,givenDateFormat);
-        LocalDate currentDate = LocalDate.parse(currentDateString,givenDateFormat);
+    /*
+        Prints the date range in which the KYC form can be submitted
+        @param  signUpDate
+                date at which the customer has signed up which is of type LocalDate
 
-        if(signUpDate.isLeapYear() && !currentDate.isLeapYear()){
-            noRange();
-            return;
-        }
+        @param  currentDate
+                date at which the customer wants to fill KYC form which is of type LocalDate
+
+        @param  givenDateFormat
+                format in which the date is to be printed which is of type DateTimeFormatter
+     */
+    static void findRange(LocalDate signUpDate,LocalDate currentDate,DateTimeFormatter givenDateFormat){
+
 
         LocalDate currentAnniversary = LocalDate.of(currentDate.getYear(),signUpDate.getMonth(),signUpDate.getDayOfMonth());
         LocalDate windowStartDate = currentAnniversary.minusDays(30);
         LocalDate windowEndDate = currentAnniversary.plusDays(30);
 
         if(currentDate.isAfter(windowStartDate)){
-
-            if(currentDate.getYear() <= signUpDate.getYear()){
-                noRange();
-                return;
-            }
 
             if(currentDate.isAfter(windowStartDate) && currentDate.isBefore(windowEndDate)){
                 System.out.println(windowStartDate.format(givenDateFormat) + " " +currentDate.format(givenDateFormat));
